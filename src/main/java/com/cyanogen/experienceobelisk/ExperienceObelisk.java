6a43e4cb3be74480e1df6a2c3c0ca18c123e8de1
cyanog3n <falseacckkys@gmail.com>
2023-05-05 23:03:12 +0800

    i forgot to register config in the mod class

--- src/main/java/com/cyanogen/experienceobelisk/ExperienceObelisk.java
+++ src/main/java/com/cyanogen/experienceobelisk/ExperienceObelisk.java
@@ -2,6 +2,7 @@ package com.cyanogen.experienceobelisk;
 
 import com.cyanogen.experienceobelisk.block.ModBlocksInit;
 import com.cyanogen.experienceobelisk.block_entities.ModTileEntitiesInit;
+import com.cyanogen.experienceobelisk.config.Config;
 import com.cyanogen.experienceobelisk.fluid.ModFluidsInit;
 import com.cyanogen.experienceobelisk.item.ModItemsInit;
 import com.cyanogen.experienceobelisk.network.PacketHandler;
@@ -9,7 +10,9 @@ import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -38,6 +41,8 @@ public class ExperienceObelisk
         ModFluidsInit.register(eventBus);
         GeckoLib.initialize();
         PacketHandler.init();
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_SPEC);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
