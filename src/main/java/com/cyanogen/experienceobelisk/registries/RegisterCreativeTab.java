b5f7a7876e36edcfb56de12eb72854134d7980bb
cyanog3n <falseacckkys@gmail.com>
2024-01-09 14:51:29 +0800

    fixed the creative mode tab having an incorrect localization tag

--- src/main/java/com/cyanogen/experienceobelisk/registries/RegisterCreativeTab.java
+++ src/main/java/com/cyanogen/experienceobelisk/registries/RegisterCreativeTab.java
@@ -4,10 +4,11 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class RegisterCreativeTab {
-    public static final CreativeModeTab MOD_TAB = new CreativeModeTab("experience_obelisk") {
+    public static final CreativeModeTab MOD_TAB = new CreativeModeTab("experienceobelisk") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(RegisterBlocks.EXPERIENCE_OBELISK.get());
         }
+
     };
 }
