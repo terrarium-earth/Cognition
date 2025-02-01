7a2431c55e05910de29f62eb634c946580577a04
cyanog3n <falseacckkys@gmail.com>
2025-02-01 19:08:49 +0800

    remove particle

--- src/main/java/com/cyanogen/experienceobelisk/block_entities/bibliophage/NutrientAgarEntity.java
+++ src/main/java/com/cyanogen/experienceobelisk/block_entities/bibliophage/NutrientAgarEntity.java
@@ -13,30 +13,4 @@ public class NutrientAgarEntity extends AbstractAgarEntity {
         super(RegisterBlockEntities.NUTRIENT_AGAR_BE.get(), pos, state);
     }
 
-    int interval = 0;
-
-    public static <T> void tick(Level level, BlockPos pos, BlockState state, T blockEntity) {
-
-        if(blockEntity instanceof NutrientAgarEntity agar){
-
-            if(!level.isClientSide && level.getGameTime() % (120 + agar.interval) == 0){
-                ServerLevel server = (ServerLevel) level;
-                double x = 0;
-                double y = 0;
-                double z = 0;
-                int count = 1;
-                double dx = 0.2;
-                double dy = 0.2;
-                double dz = 0.2;
-                double idk = 0;
-                //i want to make it occasionally emit some particle
-                //insightful_agar_outer
-                //insightful_agar_inner
-                server.sendParticles(ParticleTypes.BUBBLE, x, y, z, count, dx, dy, dz, idk);
-                agar.interval = level.random.nextIntBetweenInclusive(0,120);
-            }
-
-        }
-    }
-
 }
