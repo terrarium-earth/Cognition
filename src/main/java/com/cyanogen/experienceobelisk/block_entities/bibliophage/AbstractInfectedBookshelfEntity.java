a608b32615685ab013a12138020686b7f519e0bf
cyanog3n <falseacckkys@gmail.com>
2025-03-09 00:11:02 +0800

    testing out stacking buffs

--- src/main/java/com/cyanogen/experienceobelisk/block_entities/bibliophage/AbstractInfectedBookshelfEntity.java
+++ src/main/java/com/cyanogen/experienceobelisk/block_entities/bibliophage/AbstractInfectedBookshelfEntity.java
@@ -33,8 +33,8 @@ public abstract class AbstractInfectedBookshelfEntity extends AbstractInfectiveE
     int decayValue = 0; //the number of times a bookshelf has spawned an orb
     double infectivity = 0.02; //the chance for a bookshelf to infect another adjacent bookshelf every second
     boolean redstoneEnabled = false; //whether or not the bookshelf is sensitive to redstone. Disabled bookshelves will not infect adjacents, produce XP, or decay
-    double insightfulBonus = 1.25;
-    double extravagantBonus = 1.25;
+    double insightfulBonus = 1.20;
+    double extravagantBonus = 1.20;
 
     //-----------BEHAVIOR-----------//
 
