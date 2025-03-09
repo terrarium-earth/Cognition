98e3aa05f79605c5378c2196cc38b383d49fc15a
cyanog3n <falseacckkys@gmail.com>
2025-03-09 15:42:54 +0800

    neurogel anvil repair

--- src/main/java/com/cyanogen/experienceobelisk/item/NeurogelMendingItem.java
+++ src/main/java/com/cyanogen/experienceobelisk/item/NeurogelMendingItem.java
@@ -4,6 +4,7 @@ import com.cyanogen.experienceobelisk.registries.RegisterItems;
 import com.cyanogen.experienceobelisk.registries.RegisterSounds;
 import com.cyanogen.experienceobelisk.utils.MiscUtils;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -15,7 +16,11 @@ import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraftforge.event.ItemStackedOnOtherEvent;
+
+import java.util.Optional;
 
 public class NeurogelMendingItem extends Item {
 
@@ -25,30 +30,31 @@ public class NeurogelMendingItem extends Item {
 
     final static int repairAmount = 200;
 
-    public static ItemStack chippedAnvil = new ItemStack(Items.CHIPPED_ANVIL, 1);
-    public static ItemStack anvil = new ItemStack(Items.ANVIL, 1);
-
     public static void handleItem(ItemStackedOnOtherEvent event){
         ItemStack itemToRepair = event.getCarriedItem();
         ItemStack stackedOn = event.getStackedOnItem();
         Player player = event.getPlayer();
 
-        if(stackedOn.is(RegisterItems.MENDING_NEUROGEL.get()) && itemToRepair.isDamaged()){
+        ItemStack chippedAnvil = new ItemStack(Items.CHIPPED_ANVIL, 1);
+        ItemStack anvil = new ItemStack(Items.ANVIL, 1);
+
+        if(stackedOn.is(RegisterItems.MENDING_NEUROGEL.get())){
 
             if(itemToRepair.isDamaged()){
                 int damage = itemToRepair.getDamageValue();
                 stackedOn.shrink(1);
                 itemToRepair.setDamageValue(Math.max(damage - repairAmount, 0));
                 player.playSound(RegisterSounds.NEUROGEL_APPLY.get(), 0.75f, MiscUtils.randomInRange(0.8f, 1.2f));
+                event.setCanceled(true);
             }
             else if(itemToRepair.is(Items.CHIPPED_ANVIL)){
                 setItem(anvil, event.getSlot(), player, stackedOn, itemToRepair);
+                event.setCanceled(true);
             }
             else if(itemToRepair.is(Items.DAMAGED_ANVIL)){
                 setItem(chippedAnvil, event.getSlot(), player, stackedOn, itemToRepair);
+                event.setCanceled(true);
             }
-
-            event.setCanceled(true);
         }
 
     }
@@ -56,15 +62,14 @@ public class NeurogelMendingItem extends Item {
     public static void setItem(ItemStack item, Slot slot, Player player, ItemStack stackedOn, ItemStack itemToRepair){
 
         stackedOn.shrink(1);
+        player.playSound(RegisterSounds.NEUROGEL_APPLY.get(), 0.75f, MiscUtils.randomInRange(0.8f, 1.2f));
 
         if(itemToRepair.getCount() == 1){
             slot.set(item);
-            player.playSound(RegisterSounds.NEUROGEL_APPLY.get(), 0.75f, MiscUtils.randomInRange(0.8f, 1.2f));
         }
         else{
-            itemToRepair.shrink(1);
             player.addItem(item);
-            player.playSound(RegisterSounds.NEUROGEL_APPLY.get(), 0.75f, MiscUtils.randomInRange(0.8f, 1.2f));
+            itemToRepair.shrink(1);
         }
     }
 
@@ -80,14 +85,17 @@ public class NeurogelMendingItem extends Item {
         if(player != null && player.isShiftKeyDown() &&
                 (state.is(Blocks.CHIPPED_ANVIL) || state.is(Blocks.DAMAGED_ANVIL))){
 
-            stack.shrink(1);
+            BlockState anvil = Blocks.ANVIL.withPropertiesOf(state);
+            BlockState chipped = Blocks.CHIPPED_ANVIL.withPropertiesOf(state);
+
             if(state.is(Blocks.CHIPPED_ANVIL)){
-                level.setBlockAndUpdate(pos, Blocks.ANVIL.defaultBlockState());
+                level.setBlockAndUpdate(pos, anvil);
             }
             else if(state.is(Blocks.DAMAGED_ANVIL)){
-                level.setBlockAndUpdate(pos, Blocks.CHIPPED_ANVIL.defaultBlockState());
+                level.setBlockAndUpdate(pos, chipped);
             }
 
+            stack.shrink(1);
             player.playSound(RegisterSounds.NEUROGEL_APPLY.get(), 0.75f, MiscUtils.randomInRange(0.8f, 1.2f));
         }
 
