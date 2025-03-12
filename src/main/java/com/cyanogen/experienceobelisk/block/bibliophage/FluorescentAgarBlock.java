4ad62a9121f17c067f60b115b6c02bb9ad540000
cyanog3n <falseacckkys@gmail.com>
2025-03-12 23:59:19 +0800

    thats the last change for today

--- src/main/java/com/cyanogen/experienceobelisk/block/bibliophage/FluorescentAgarBlock.java
+++ src/main/java/com/cyanogen/experienceobelisk/block/bibliophage/FluorescentAgarBlock.java
@@ -3,9 +3,15 @@ package com.cyanogen.experienceobelisk.block.bibliophage;
 import com.cyanogen.experienceobelisk.block_entities.bibliophage.FluorescentAgarEntity;
 import com.cyanogen.experienceobelisk.block_entities.bibliophage.NutrientAgarEntity;
 import com.cyanogen.experienceobelisk.registries.RegisterBlockEntities;
+import com.cyanogen.experienceobelisk.registries.RegisterBlocks;
+import com.cyanogen.experienceobelisk.registries.RegisterItems;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.ExperienceOrb;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
@@ -15,6 +21,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.EntityCollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -41,6 +48,21 @@ public class FluorescentAgarBlock extends HalfTransparentBlock implements Entity
             }
         }
         return super.getCollisionShape(state, getter, pos, context);
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
+
+        if(player.getItemInHand(hand).is(RegisterItems.ATTUNEMENT_STAFF.get()) && level.getBlockEntity(pos) instanceof FluorescentAgarEntity agar){
+
+            Component message = Component.translatable("message.experienceobelisk.binding_wand.query_fluorescent_agar", agar.getInfectionProgress());
+            if(!level.isClientSide){
+                player.displayClientMessage(message, true);
+            }
+            return InteractionResult.sidedSuccess(level.isClientSide);
+        }
+
+        return super.use(state, level, pos, player, hand, result);
     }
 
     //-----BLOCK ENTITY-----//
