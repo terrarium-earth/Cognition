4242408b7806b1cc8b5fe901f0ee9d1faedc966f
cyanog3n <falseacckkys@gmail.com>
2025-05-14 20:01:51 +0800

    fix transforming focus dupe

--- src/main/java/com/cyanogen/cognition/block_entities/MolecularMetamorpherEntity.java
+++ src/main/java/com/cyanogen/cognition/block_entities/MolecularMetamorpherEntity.java
@@ -350,7 +350,7 @@ public class MolecularMetamorpherEntity extends ExperienceReceivingEntity implem
                 if(ingredient.test(stack)){
 
                     if(stack.is(RegisterItems.TRANSFORMING_FOCUS.get())){ //todo: replace this with a tag check
-                        if(stack.getDamageValue() >= TransformingFocusItem.durability - 1){
+                        if(stack.getDamageValue() >= TransformingFocusItem.durability - 1 || recipe.isNameFormatting()){
                             stack.shrink(1);
                         }
                         else{
