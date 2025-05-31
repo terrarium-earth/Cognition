8673347327ff7425d68f5d41b4aee0425facb8f0
cyanog3n <falseacckkys@gmail.com>
2025-05-31 16:19:57 +0800

    Flasks of Power now use vanilla bucket behavior where possible

--- src/main/java/com/cyanogen/experienceobelisk/item/FlaskPoseidonItem.java
+++ src/main/java/com/cyanogen/experienceobelisk/item/FlaskPoseidonItem.java
@@ -3,12 +3,20 @@ package com.cyanogen.experienceobelisk.item;
 import com.cyanogen.experienceobelisk.registries.RegisterSounds;
 import com.cyanogen.experienceobelisk.utils.ExperienceUtils;
 import com.cyanogen.experienceobelisk.utils.MiscUtils;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.AbstractCauldronBlock;
 import net.minecraft.world.level.block.Blocks;
@@ -17,14 +25,17 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 
-public class FlaskPoseidonItem extends Item{
+public class FlaskPoseidonItem extends BucketItem {
 
     public FlaskPoseidonItem(Properties p) {
-        super(p);
+        super(Fluids.WATER.getSource(), p);
     }
 
     @Override
@@ -39,76 +50,109 @@ public class FlaskPoseidonItem extends Item{
     private final FluidStack fluidStack = new FluidStack(Fluids.WATER.getSource(), 1000);
 
     @Override
-    public InteractionResult onItemUseFirst(ItemStack stack, UseOnContext context) {
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        //for regular placement & waterlogging
+        //uses vanilla bucket behavior
 
-        BlockPos clickedPos = context.getClickedPos(); //the position of the block that was clicked
-        BlockPos replacePos = context.getClickedPos().relative(context.getClickedFace(), 1); //the position adjacent to the clicked block
+        ItemStack itemstack = player.getItemInHand(hand);
+        BlockHitResult blockhitresult = getPlayerPOVHitResult(
+                level, player, ClipContext.Fluid.NONE
+        );
+
+        if(blockhitresult.getType() == HitResult.Type.MISS){
+            return InteractionResultHolder.pass(itemstack);
+        }
+        else if(blockhitresult.getType() != HitResult.Type.BLOCK){
+            return InteractionResultHolder.pass(itemstack);
+        }
+        else{
+            BlockPos blockpos = blockhitresult.getBlockPos();
+            Direction direction = blockhitresult.getDirection();
+            BlockPos blockpos1 = blockpos.relative(direction);
+
+            if(!level.mayInteract(player, blockpos) || !player.mayUseItemAt(blockpos1, direction, itemstack)){
+                return InteractionResultHolder.fail(itemstack);
+            }
+            else if(canUse(player)){
+                BlockState blockstate = level.getBlockState(blockpos);
+                BlockPos blockpos2 = canBlockContainFluid(level, blockpos, blockstate) ? blockpos : blockpos1;
+
+                if(this.emptyContents(player, level, blockpos2, blockhitresult, itemstack)){
+                    this.checkExtraContent(player, level, itemstack, blockpos2);
+                    if(player instanceof ServerPlayer){
+                        CriteriaTriggers.PLACED_BLOCK.trigger((ServerPlayer)player, blockpos2, itemstack);
+                    }
+
+                    player.awardStat(Stats.ITEM_USED.get(this));
+                    handlePlayer(player);
+
+                    return InteractionResultHolder.sidedSuccess(itemstack, level.isClientSide());
+                }
+                else{
+                    return InteractionResultHolder.fail(itemstack);
+                }
+            }
+            return InteractionResultHolder.fail(itemstack);
+        }
+    }
+
+    @Override
+    public InteractionResult onItemUseFirst(ItemStack stack, UseOnContext context) {
+        //for cauldrons and fluid containers
+
+        BlockPos clickedPos = context.getClickedPos();
         Level level = context.getLevel();
         Player player = context.getPlayer();
+        Direction direction = context.getClickedFace();
 
-        if(player != null && (player.isCreative() || ExperienceUtils.getTotalXP(player) >= cost) && !player.getCooldowns().isOnCooldown(this)){
-
+        if(player != null && canUse(player)){
             boolean canModifyClicked = level.mayInteract(player, clickedPos) && player.mayUseItemAt(clickedPos, context.getClickedFace(), player.getItemInHand(context.getHand()));
-            boolean canPlace = level.mayInteract(player, replacePos) && player.mayUseItemAt(replacePos, context.getClickedFace(), player.getItemInHand(context.getHand()));
             boolean edit = !player.isShiftKeyDown() && canModifyClicked;
-
             BlockState clickedState = level.getBlockState(clickedPos);
-            BlockState stateToReplace = level.getBlockState(replacePos);
 
-            if(clickedState.getBlock() instanceof AbstractCauldronBlock && edit){ //cauldrons
+            if(clickedState.getBlock() instanceof AbstractCauldronBlock && edit){
 
                 if(clickedState.getBlock().equals(Blocks.CAULDRON)){
                     level.setBlockAndUpdate(clickedPos, Blocks.WATER_CAULDRON.defaultBlockState().trySetValue(BlockStateProperties.LEVEL_CAULDRON, 3));
-                    return handlePlayer(player, level);
+                    handlePlayer(player);
+                    return InteractionResult.sidedSuccess(level.isClientSide);
                 }
                 else{
                     return InteractionResult.FAIL;
                 }
             }
-            else if(clickedState.getBlock() instanceof LiquidBlockContainer container && edit){ //waterloggable blocks
-                if(container.canPlaceLiquid(level, clickedPos, clickedState, Fluids.WATER.getSource())){
-                    container.placeLiquid(level, clickedPos, clickedState, Fluids.WATER.getSource().defaultFluidState());
-                }
-
-                return handlePlayer(player, level);
-            }
-            else if(clickedState.hasBlockEntity() && edit){ //fluid containers
+            else if(clickedState.hasBlockEntity() && edit){
 
                 BlockEntity entity = level.getBlockEntity(clickedPos);
                 assert entity != null;
-                if(entity.getCapability(ForgeCapabilities.FLUID_HANDLER).resolve().isPresent()){
-                    IFluidHandler handler = entity.getCapability(ForgeCapabilities.FLUID_HANDLER).resolve().get();
 
+                LazyOptional<IFluidHandler> handlerOptional = level.getCapability(ForgeCapabilities.FLUID_HANDLER, direction);
+
+                if(handlerOptional.resolve().isPresent()){
+                    IFluidHandler handler = handlerOptional.resolve().get();
                     int drainAmount = handler.fill(fluidStack, IFluidHandler.FluidAction.SIMULATE);
 
                     if(drainAmount != 0){
                         handler.fill(fluidStack, IFluidHandler.FluidAction.EXECUTE);
-                        return handlePlayer(player, level);
+                        handlePlayer(player);
+                        return InteractionResult.sidedSuccess(level.isClientSide);
                     }
                 }
-            }
-            else if((stateToReplace.isAir() || stateToReplace.canBeReplaced(Fluids.WATER)) && canPlace){ //air or replaceable block
-                if(level.dimensionType().ultraWarm()){
-                    Fluids.WATER.getFluidType().onVaporize(player, level, replacePos, null);
-                }
-                else{
-                    level.setBlockAndUpdate(replacePos, Blocks.WATER.defaultBlockState());
-                }
-
-                return handlePlayer(player, level);
             }
         }
 
         return super.useOn(context);
     }
 
-    public InteractionResult handlePlayer(Player player, Level level){
+    public boolean canUse(Player player){
+        return (player.isCreative() || ExperienceUtils.getTotalXP(player) >= cost) && !player.getCooldowns().isOnCooldown(this);
+    }
 
+    public void handlePlayer(Player player){
         int k = player.isCreative() ? 0 : 1;
         player.getCooldowns().addCooldown(this, cooldown);
         player.giveExperiencePoints(-cost * k);
         player.playSound(RegisterSounds.FLASK_EMPTY_WATER.get(), MiscUtils.randomInRange(0.8f, 1.0f), MiscUtils.randomInRange(0.8f, 1.0f));
-        return InteractionResult.sidedSuccess(level.isClientSide);
     }
 
 }
